4edfe4afeeffcab71f164eb76de801ebf5058cea
ganesh <ganesharavindhkce@gmail.com>
2018-08-18 16:34:46 +0530

    18-8-2018

---
+++ webpage/in_cam1.py
@@ -0,0 +1,23 @@
+import cv2
+
+class VideoCamera1(object):
+    def __init__(self):
+        # Using OpenCV to capture from device 0. If you have trouble capturing
+        # from a webcam, comment the line below out and use a video file
+        # instead.
+        self.video = cv2.VideoCapture("rtsp://admin:admin0864@121.6.207.205:8081/cam/realmonitor?channel=1&subtype=1")
+        #self.video = cv2.VideoCapture('/home/katomaran/Desktop/flask_app/video_streaming_with_flask_example-master/in_cam_video.mkv')
+        # If you decide to use video.mp4, you must have this file in the folder
+        # as the main.py.
+        # self.video = cv2.VideoCapture('video.mp4')
+    
+    def __del__(self):
+        self.video.release()
+    
+    def get_frame(self):
+        success, image = self.video.read()
+        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
+        # so we must encode it into JPEG in order to correctly display the
+        # video stream.)
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
