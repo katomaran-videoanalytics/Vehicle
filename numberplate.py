0741d53a47c77768e379350db6d6c55fd167768c
sksenthilkumar <sksenthilkumar93@gmail.com>
2018-08-21 09:02:28 +0200

    refactoring

--- numberplate.py
+++ numberplate.py
@@ -22,11 +22,11 @@ def main():
             for detection in cvOut[0, 0, :, :]:
                 score = float(detection[2])
                 if score > 0.9:
-                    left = detection[3] * cols
-                    top = detection[4] * rows
-                    right = detection[5] * cols
-                    bottom = detection[6] * rows
-                    cv.rectangle(img, (int(left), int(top)), (int(right), int(bottom)), (23, 230, 210), thickness=2)
+                    left = int(detection[3] * cols)
+                    top = int(detection[4] * rows)
+                    right = int(detection[5] * cols)
+                    bottom = int( detection[6] * rows)
+                    cv.rectangle(img, left, top, (right, bottom), (23, 230, 210), thickness=2)
 
             cv.imshow('img', img)
             if cv.waitKey(1) and 0xFF == ord('q'):
