066102b1003fa9c8b3f486d6329cb73bae6b54d3
sksenthilkumar <sksenthilkumar93@gmail.com>
2018-08-21 08:57:25 +0200

    refactoring

--- numberplate.py
+++ numberplate.py
@@ -1,27 +1,37 @@
 import cv2 as cv
 
-cvNet = cv.dnn.readNetFromTensorflow('models/Number_plate_Model/frozen_inference_graph.pb',
-                                     'models/Number_plate_Model/NuPlate_model.pbtxt')
+# configuration files
+model_pb = 'models/Number_plate_Model/frozen_inference_graph.pb'
+model_pbtxt = 'models/Number_plate_Model/NuPlate_model.pbtxt'
+ip_cam_URL = 'rtsp://admin:admin0864@121.6.207.205:8081/cam/realmonitor?channel=1&subtype=1'
 
-cap1 = cv.VideoCapture("rtsp://admin:admin0864@121.6.207.205:8081/cam/realmonitor?channel=1&subtype=1")
 
-while True:
-    ret, img = cap1.read()
-    if ret is True:
-        rows = img.shape[0]
-        cols = img.shape[1]
-        cvNet.setInput(
-            cv.dnn.blobFromImage(img, 1.0 / 127.5, (300, 300), (127.5, 127.5, 127.5), swapRB=True, crop=False))
-        cvOut = cvNet.forward()
-        for detection in cvOut[0, 0, :, :]:
-            score = float(detection[2])
-            if score > 0.9:
-                left = detection[3] * cols
-                top = detection[4] * rows
-                right = detection[5] * cols
-                bottom = detection[6] * rows
-                cv.rectangle(img, (int(left), int(top)), (int(right), int(bottom)), (23, 230, 210), thickness=2)
+cvNet = cv.dnn.readNetFromTensorflow(model_pb, model_pbtxt)
+cap = cv.VideoCapture(ip_cam_URL)
 
-        cv.imshow('img', img)
-        if cv.waitKey(1) and 0xFF == ord('q'):
-            break
+
+def main():
+    while True:
+        ret, img = cap.read()
+        if ret is True:
+            rows = img.shape[0]
+            cols = img.shape[1]
+            ip_for_nn = cv.dnn.blobFromImage(img, 1.0 / 127.5, (300, 300), (127.5, 127.5, 127.5), swapRB=True, crop=False)
+            cvNet.setInput(ip_for_nn)
+            cvOut = cvNet.forward()
+            for detection in cvOut[0, 0, :, :]:
+                score = float(detection[2])
+                if score > 0.9:
+                    left = detection[3] * cols
+                    top = detection[4] * rows
+                    right = detection[5] * cols
+                    bottom = detection[6] * rows
+                    cv.rectangle(img, (int(left), int(top)), (int(right), int(bottom)), (23, 230, 210), thickness=2)
+
+            cv.imshow('img', img)
+            if cv.waitKey(1) and 0xFF == ord('q'):
+                break
+
+
+if __name__ == '__main__':
+    main()
