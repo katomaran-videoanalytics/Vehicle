1ea950664265e9d9a7b57f9a980d909a662132af
ganesharavindh <ganesharavindhkce@gmail.com>
2018-08-30 15:28:31 +0530

    30-8-2018-Project-95%

--- number.py
+++ number.py
@@ -1,27 +1,80 @@
 import cv2 as cv
+
 import io
+import re
+
 from google.cloud import storage
 from google.cloud import vision
 from google.protobuf import json_format
 
 cvNet = cv.dnn.readNetFromTensorflow('models/Number_plate_Model/frozen_inference_graph.pb', 'models/Number_plate_Model/NuPlate_model.pbtxt')
 
-def detect_text(path):
-	"""Detects text in the file."""
+def denoising(path):
+	img = cv.imread('Results/gray_image.png')
+	b,g,r = cv.split(img)           # get b,g,r
+	rgb_img = cv.merge([r,g,b])     # switch it to rgb
+
+	# Denoising
+	dst = cv.fastNlMeansDenoising(img,None,10,7,21) #For RGB-cv2.fastNlMeansDenoisingColored(img,None,10,10,7,21)
+
+	b,g,r = cv.split(dst)           # get b,g,r
+	rgb_dst = cv.merge([r,g,b])     # switch it to rgb
+	cv.imwrite('Results/denoising.png',rgb_dst)
+	return rgb_dst
+
+
+def conv_grayscale(path):
+	image = cv.imread('Results/final.jpg')
+	gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+	cv.imwrite('Results/gray_image.png',gray_image)
+	return denoising('Results/gray_image.png')
+	
+	
+	  
+def increse_contrast(path):
+	img = cv.imread(path)
+
+	lab= cv.cvtColor(img, cv.COLOR_BGR2LAB)
+
+	l, a, b = cv.split(lab)
+
+	clahe = cv.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
+	cl = clahe.apply(l)
+
+	limg = cv.merge((cl,a,b))
+
+	final = cv.cvtColor(limg, cv.COLOR_LAB2BGR)
+	cv.imwrite('Results/final.jpg',final)
+	return conv_grayscale('Results/final.jpg')
+	
+def detect_document(path):
+	"""Detects document features in an image."""
 	client = vision.ImageAnnotatorClient()
 
+	# [START vision_python_migration_document_text_detection]
 	with io.open(path, 'rb') as image_file:
 		content = image_file.read()
-	text1=None
 
 	image = vision.types.Image(content=content)
 
-	response = client.text_detection(image=image)
-	texts = response.text_annotations
-	for text in texts:
-		text1=text.description
-	return text1
-
+	response = client.document_text_detection(image=image)
+	stri=''
+	sav=0
+	count=0
+	for page in response.full_text_annotation.pages:
+		for block in page.blocks:
+			for paragraph in block.paragraphs:
+				for word in paragraph.words:
+					word_text = ''.join([symbol.text for symbol in word.symbols])
+					#print('Word text: {} (confidence: {})'.format(word_text, word.confidence))
+					stri+=word_text
+					sav=sav+word.confidence
+					count=count+1
+					#print(stri)
+	if count==0:
+		return (None,None)
+	return (stri,abs(sav/count))
+					
 def number_detect(frame):
 	img = cv.imread(frame)
 	rows = img.shape[0]
@@ -35,9 +88,16 @@ def number_detect(frame):
 			top = int(detection[4] * rows)
 			right = int(detection[5] * cols)
 			bottom = int( detection[6] * rows)
-			cv.rectangle(img, (int(left), int(top)), (int(right), int(bottom)), (23, 230, 210), thickness=2)
+			#cv.rectangle(img, (int(left), int(top)), (int(right), int(bottom)), (23, 230, 210), thickness=2)
 			img4=img[top:bottom,left:right]
-	cv.imwrite("images/num.jpg",img4)
-	return detect_text("images/num.jpg")
+			cv.imwrite("images/num.jpg",img4)
+			cont=increse_contrast("images/num.jpg")
+			cv.imwrite('gimp.png',cont)
+			return detect_document('gimp.png')
+	return (None,None)
 			
+
+			
+					
+
 
--- testing.py
+++
@@ -1,4 +0,0 @@
-from number import number_detect
-
-path="/home/katomaran/Desktop/0000000310.png"
-print(number_detect(path))
